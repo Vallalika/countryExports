f1ad1f819dbe436a8f336a85214d9b0f55720e9d
Valerie Bayard <valerie.bayard@gmail.com>
2022-10-10 22:05:15 +0100

    fixing issues between remote and llocal branches

--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -6,10 +6,10 @@ public class Runner {
         FileResource fr = new FileResource();
         CSVParser parser = fr.getCSVParser();
         Query testQuery = new Query();
-//        testQuery.countryInfo(parser,"Madagascar");
+//        testQuery.countryInfo(parser,"Nauru");
 //        testQuery.listExportersTwoProducts(parser, "gold", "diamonds");
 //        testQuery.numberOfExporters(parser,"gold");
-        testQuery.bigExporters(parser,"$999,999,999");
+        testQuery.bigExporters(parser,"$999,999,999,999");
     }
 
 
